96026fc95360a4e9628d1bf3432a6a8ec6b23570
Chao Zhang <czhang@axonify.com>
2019-11-29 21:16:58 -0500

    Add 2015 Day6

---
+++ 2015/6A.js
@@ -0,0 +1,48 @@
+function A(input) {
+  let grid = [...Array(1000)].map(x => Array(1000).fill(0));
+
+  input.split('\n').forEach(string => {
+    if (string[4] !== ' ') {
+      string = string.slice(0, 4) + ' ' + string.slice(4);
+    }
+
+    parts = string.split(' ');
+
+    grid = takeAction(grid, parts[1], parts[2].split(','), parts[4].split(','));
+  });
+
+  return grid.reduce((prev, curr) => {
+    return prev + curr.reduce((prev, curr) => {
+      return prev + curr;
+    });
+  }, 0);
+}
+
+function takeAction(grid, action, start, end) {
+  start[0] = parseInt(start[0], 10);
+  start[1] = parseInt(start[1], 10);
+  end[0] = parseInt(end[0], 10);
+  end[1] = parseInt(end[1], 10);
+
+  if (action === 'on') {
+    for (let i = start[0]; i < end[0] + 1; i++) {
+      for (let j = start[1]; j < end[1] + 1; j++) {
+        grid[i][j] = 1;
+      }
+    }
+  } else if (action === 'off') {
+    for (let i = start[0]; i < end[0] + 1; i++) {
+      for (let j = start[1]; j < end[1] + 1; j++) {
+        grid[i][j] = 0;
+      }
+    }
+  } else {
+    for (let i = start[0]; i < end[0] + 1; i++) {
+      for (let j = start[1]; j < end[1] + 1; j++) {
+        grid[i][j] = (grid[i][j] + 1) % 2;
+      }
+    }
+  }
+
+  return grid;
+}
