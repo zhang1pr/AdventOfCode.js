026dd5748dec842a688c391d7d0cbd40ae7b7c19
Chao Zhang <czhang@axonify.com>
2019-11-29 19:33:04 -0500

    Add 2015 Day4

---
+++ 2015/4B.js
@@ -0,0 +1,14 @@
+function A(input) {
+  const number = 0;
+  let hash = input;
+
+  // Import SparkMD5
+  // https://cdnjs.cloudflare.com/ajax/libs/q.js/1.4.1/q.js
+  // https://cdnjs.cloudflare.com/ajax/libs/spark-md5/2.0.2/spark-md5.min.js
+  while (!SparkMD5.hash(hash).startsWith('000000')) {
+    number += 1;
+    hash = input + number.toString();
+  }
+
+  return number;
+}
