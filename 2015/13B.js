48fdbbc46ca7d2b05e2491c93297d00f8b548e6b
Chao Zhang <czhang@axonify.com>
2019-12-01 16:26:21 -0500

    Add 2015 Day13

---
+++ 2015/13B.js
@@ -0,0 +1,45 @@
+function A(input) {
+  const map = new Map();
+  const people = [];
+  const seating = [];
+
+  input.split('\n').forEach(string => {
+    const parts = string.split(' ');
+    const result = parts[2] === 'gain' ? parseInt(parts[3]) : - parseInt(parts[3]);
+    const person1 = parts[0];
+    const person2 = parts[parts.length-1].split('.')[0];
+
+    map.set(person1 + '-' + person2, result);
+
+    if (!people.includes(parts[0])) {
+      people.push(parts[0]);
+    }
+  });
+
+  return optimizeSeating(people.length, people, seating, map);
+}
+
+function optimizeSeating(total, people, seating, map) {
+  if (people.length === 1) {
+    const person0 = people[0];
+    seating.push(person0);
+    const person1 = seating[total-2];
+
+    return map.get(person1 + '-' + person0) + map.get(person0 + '-' + person1);
+  } else {
+    let count = -Infinity;
+
+    for (let i=0; i<people.length; i++) {
+      const person = people[i];
+      seating[total-people.length] = person;
+      const personLeft = seating[total-people.length-1];
+
+      const next = optimizeSeating(total, people.filter(p => p !== people[i]), seating.slice(), map);
+      const current = people.length === total ? 0 : map.get(person + '-' + personLeft) + map.get(personLeft + '-' + person);
+
+      count = Math.max(count, current + next);
+    }
+
+    return count;
+  }
+}
