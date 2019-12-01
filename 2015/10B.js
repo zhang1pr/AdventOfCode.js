54ed90eca93e388cdecf0339712dab22fb6c897f
Chao Zhang <czhang@axonify.com>
2019-11-30 21:19:19 -0500

    Add 2015 Day10

---
+++ 2015/10B.js
@@ -0,0 +1,33 @@
+function A(input) {
+  let string = input;
+  let result;
+  let count;
+  let letter;
+
+  for (let i=0; i<50; i++) {
+    string = result || input;
+    result = '';
+
+    count = 0;
+    letter = null;
+
+    for (let i=0; i<string.length; i++) {
+      if (letter == null) {
+        count += 1;
+        letter = string[i];
+      } else if (string[i] === letter) {
+        count += 1;
+      } else {
+        result = result + count.toString() + letter;
+        letter = string[i];
+        count = 1;
+      }
+
+      if (i === string.length-1) {
+        result = result + count.toString() + letter;
+      }
+    }
+  }
+
+  return result.length;
+}
