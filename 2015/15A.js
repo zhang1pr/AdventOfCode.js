e7cae2766ae5f3de730e44339da9a7fc7b3cf59f
Chao Zhang <czhang@axonify.com>
2019-12-01 23:00:01 -0500

    Add 2015 Day15

---
+++ 2015/15A.js
@@ -0,0 +1,48 @@
+function A(input) {
+  const materials = []
+
+  input.split('\n').forEach(material => {
+    materials.push([]);
+
+    material.split(',').forEach(string => {
+      strings = string.split(' ');
+
+      materials[materials.length-1].push(parseInt(strings[strings.length-1]));
+    });
+  });
+
+  return calculateScore(materials, materials.slice(), 100, []);
+}
+
+function calculateScore(materials, typeRemaining, remainder, numbers) {
+  if (typeRemaining.length === 1) {
+    numbers.push(remainder);
+
+    const scores = Array(numbers.length).fill(0);
+
+    for (i = 0; i < numbers.length; i++) {
+      for (s = 0; s < numbers.length; s++) {
+        scores[s] += numbers[i]* materials[i][s];
+      }
+    }
+
+    return scores.reduce((prev, curr) => {
+      if (curr <= 0) {
+        return 0;
+      } else {
+        return prev * curr;
+      }
+    }, 1);
+  }
+
+  let max = -Infinity;
+  typeRemaining = typeRemaining.slice(1);
+
+  for (let i = 0; i < remainder+1; i++) {
+    const numbersCopy = numbers.slice();
+    numbersCopy.push(i);
+    max = Math.max(max, calculateScore(materials, typeRemaining, remainder - i, numbersCopy));
+  }
+
+  return max;
+}
