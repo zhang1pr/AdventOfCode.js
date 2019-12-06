0dc283f62d284ea9801a547f07c727c7a70c384a
Chao Zhang <czhang@axonify.com>
2019-12-06 13:32:06 -0500

    Add 2016 Day5

---
+++ 2016/5A.js
@@ -0,0 +1,22 @@
+function A(input) {
+  // Import SparkMD5
+  // https://cdnjs.cloudflare.com/ajax/libs/q.js/1.4.1/q.js
+  // https://cdnjs.cloudflare.com/ajax/libs/spark-md5/2.0.2/spark-md5.min.js
+
+  let number = 0;
+  let target = input + '0';
+  let result = '';
+
+  while(result.length < 8) {
+    hash = SparkMD5.hash(target);
+
+    if (hash.startsWith('00000')) {
+      result = result.concat(hash[5]);
+    }
+
+    number += 1;
+    target = input + number.toString();
+  }
+
+  return result;
+}
---
+++ 2016/5B.js
@@ -0,0 +1,26 @@
+function B(input) {
+  // Import SparkMD5
+  // https://cdnjs.cloudflare.com/ajax/libs/q.js/1.4.1/q.js
+  // https://cdnjs.cloudflare.com/ajax/libs/spark-md5/2.0.2/spark-md5.min.js
+
+  let number = 0;
+  let target = input + '0';
+  let result = [];
+
+  while (result.filter(Boolean).length < 8) {
+    hash = SparkMD5.hash(target);
+
+    if (hash.startsWith('00000') && !Number.isNaN(parseInt(hash[5], 10)) && parseInt(hash[5], 10) >= 0 && parseInt(hash[5], 10) <= 7) {
+      const position = parseInt(hash[5], 10);
+
+      if (position != null && !result[position]) {
+        result[position] = hash[6];
+      }
+    }
+
+    number += 1;
+    target = input + number.toString();
+  }
+
+  return result.join('');
+}
