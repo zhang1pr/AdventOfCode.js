cb123addac119aa511fc045d0a6cef9dcba0b6fe
Chao Zhang <czhang@axonify.com>
2019-12-07 14:34:17 -0500

    Change 2015 Day4

--- 2015/4B.js
+++ 2015/4B.js
@@ -1,4 +1,4 @@
-function A(input) {
+function B(input) {
   // Import SparkMD5
   // https://cdnjs.cloudflare.com/ajax/libs/q.js/1.4.1/q.js
   // https://cdnjs.cloudflare.com/ajax/libs/spark-md5/2.0.2/spark-md5.min.js
