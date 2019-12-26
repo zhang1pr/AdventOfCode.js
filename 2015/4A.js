5dbe02d260063b9cec7f565a1e642345cd6dddbf
Chao Zhang <czhang@axonify.com>
2019-12-26 16:23:18 -0500

    Change 2015 Day4

--- 2015/4A.js
+++ 2015/4A.js
@@ -3,8 +3,8 @@ function A(input) {
   // https://cdnjs.cloudflare.com/ajax/libs/q.js/1.4.1/q.js
   // https://cdnjs.cloudflare.com/ajax/libs/spark-md5/2.0.2/spark-md5.min.js
 
-  let number = 0;
-  let target = input;
+  let number = 1;
+  let target = input + number.toString();
 
   while (!SparkMD5.hash(target).startsWith('00000')) {
     number += 1;
--- 2015/4B.js
+++ 2015/4B.js
@@ -3,8 +3,8 @@ function B(input) {
   // https://cdnjs.cloudflare.com/ajax/libs/q.js/1.4.1/q.js
   // https://cdnjs.cloudflare.com/ajax/libs/spark-md5/2.0.2/spark-md5.min.js
 
-  let number = 0;
-  let target = input;
+  let number = 1;
+  let target = input + number.toString();
 
   while (!SparkMD5.hash(target).startsWith('000000')) {
     number += 1;
