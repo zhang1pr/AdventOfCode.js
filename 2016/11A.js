857cbaf2eeb0c87e664e3fdcca255726ac0e074d
Chao Zhang <czhang@axonify.com>
2019-12-26 00:04:43 -0500

    Add 2016 Day11

---
+++ 2016/11A.js
@@ -0,0 +1,54 @@
+function B(input) {
+  const floors = [];
+  let steps = 0;
+
+  input.split('\n').forEach(line => {
+    let count = 0;
+
+    const generatorRegex = /\w+ generator/g;
+    const microchipRegex = /\w+-\w+ microchip/g;
+
+    let generators = line.match(generatorRegex) || [];
+    let microchips = line.match(microchipRegex) || [];
+
+    count += generators.length + microchips.length;
+
+    floors.push(count);
+  });
+
+  const total = floors.reduce((prev, curr) => prev + curr);
+  let elevator = Math.min(floors[0], 2);
+  let floor = 0;
+  floors[0] -= elevator;
+
+  while (floors[3] + 1 !== total) {
+    while (elevator < 2 && floor > 0) {
+      floor--;
+      const carry = Math.min(floors[floor], 2 - elevator);
+
+      if (carry > 0) {
+        elevator += carry;
+        floors[floor] -= carry;
+      }
+
+      steps++;
+    }
+
+    while (floor < 3) {
+      floor++;
+      const carry = Math.min(floors[floor], 2 - elevator);
+
+      if (carry > 0) {
+        elevator += carry;
+        floors[floor] -= carry;
+      }
+
+      steps++;
+    }
+
+    floors[3] += 1;
+    elevator--;
+  }
+
+  return steps;
+}
