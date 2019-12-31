4c897b8489c308ca29b2099dea2eb5b84a9330a7
Chao Zhang <czhang@axonify.com>
2019-12-30 22:46:32 -0500

    Add 2016 Day18

---
+++ 2016/18B.js
@@ -0,0 +1,25 @@
+function B(input) {
+  let level = 1;
+  let count = input.match(/\./g).length;
+
+  while (level < 400000) {
+    level++;
+
+    let newInput = '';
+    for (let i = 0; i < input.length; i++) {
+      if (
+        input[i-1] !== '^' && input[i+1] === '^'
+        || input[i-1] === '^' && input[i+1] !== '^'
+      ) {
+        newInput += '^';
+      } else {
+        newInput += '.';
+        count++;
+      }
+    }
+
+    input = newInput;
+  }
+
+  return count;
+}
