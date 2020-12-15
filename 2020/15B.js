41c34498f1cd159e2ac48138180f8ff33b81aa82
Chao Zhang <czhang@axonify.com>
2020-12-15 00:22:55 -0500

    Add 2020 Day15

---
+++ 2020/15B.js
@@ -0,0 +1,33 @@
+function B(input) {
+  const map = new Map();
+
+  const nums = input.split(',').map(a => Number(a));
+  let recent;
+ 
+  let i = 0;
+  for (; i < nums.length; i++) {
+    if (!map.has(nums[i])) {
+      map.set(nums[i], []);
+    }
+
+    map.get(nums[i]).push(i);
+    recent = nums[i];
+  }
+
+  for (; i < 2020; i++) {
+    const get = map.get(recent);
+    if (get.length == 1) {
+      recent = 0;
+    } else {
+      recent = get[get.length - 1] - get[get.length - 2];
+    }
+
+    if (!map.has(recent)) {
+      map.set(recent,[]);
+    }
+
+    map.get(recent).push(i);
+  }
+
+  return recent;
+}
