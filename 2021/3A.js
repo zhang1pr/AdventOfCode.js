cdc67213c287ecd2d50aa368e51e66c4685dd8ec
Chao Zhang <czhang@axonify.com>
2021-12-04 01:00:22 -0500

    Add 2021 day3

---
+++ 2021/3A.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.split('\n').map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => a.split(/\s+/).map(a => Number(a)));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let arr = readword(input);
+  let tmp = '', tmp2 = '';
+  let list = Array(arr[0].length).fill(0);
+  
+  for (let i=0;i<arr.length;i++) {
+    let cur = arr[i];
+    
+    for (let j=0;j<cur.length;j++) {
+      if (cur[j] == '1') {
+        list[j]++;
+      }
+    }
+  }
+
+  for (let l of list) {
+    if (l > arr.length - l) {
+      tmp += '1';
+      tmp2 += '0';
+    } else {
+      tmp += '0';
+      tmp2 += '1'
+    }
+  }
+
+  return parseInt(tmp,2) * parseInt(tmp2,2);
+}
---
+++ 2021/3B.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.split('\n').map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => a.split(/\s+/).map(a => Number(a)));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let arr = readword(input);
+  let tmp = '', tmp2 = '';
+  
+  let carr = arr.slice();
+  let i = 0;
+  while (carr.length > 1) { 
+    let cnt = 0;
+    let narr = [];
+    
+    for (let cur of carr) {
+      if (cur[i] == '1') {
+        cnt++;
+      }
+    }
+
+    for (let cur of carr) {
+      if ((cur[i] == 1) ^ (cnt < carr.length - cnt)) {
+        narr.push(cur);
+      }
+    }
+
+    carr = narr;
+    i++;
+  }
+  tmp = carr[0];
+
+  carr = arr;
+  i = 0;
+  while (carr.length > 1) { 
+    let cnt = 0;
+    let narr = [];
+    
+    for (let cur of carr) {
+      if (cur[i] == '1') {
+        cnt++;
+      }
+    }
+
+    for (let cur of carr) {
+      if ((cur[i] == 1) ^ (cnt >= carr.length - cnt)) {
+        narr.push(cur);
+      }
+    }
+
+    carr = narr;
+    i++;
+  }
+
+  tmp2 = carr[0];
+  return parseInt(tmp,2) * parseInt(tmp2,2);
+}
