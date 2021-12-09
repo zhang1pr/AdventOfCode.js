50394c018668fe5366b5e35bf4565916074a9ec5
Chao Zhang <czhang@axonify.com>
2021-12-09 00:33:03 -0500

    Add 2021 day9

---
+++ 2021/9A.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.split('\n').map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => a.split(/\s+/).map(a => Number(a)));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let res = 0;
+  let arr = readword(input).map(a => a.split('').map(a=>+a)); 
+
+  for (let i=0;i<arr.length;i++) {
+    let cur = arr[i];
+
+    for (let j=0;j<cur.length;j++) {
+      let flag = true;
+
+      for (let [di,dj] of darr) {
+        let ni = i+di, nj=j+dj;
+        
+        if (arr[ni] == null || arr[ni][nj] == null) continue;
+      
+        if (arr[ni][nj] <= cur[j]) flag = false;
+      }
+
+      if (flag) res = res + 1 + cur[j];
+    }
+  }
+
+  return res;
+}
---
+++ 2021/9B.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.split('\n').map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => a.split(/\s+/).map(a => Number(a)));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let set = new Set();
+  let arr = readword(input).map(a => a.split('').map(a=>+a)); 
+  let a = [];
+
+  for (let i=0;i<arr.length;i++) {
+    let cur = arr[i];
+
+    for (let j=0;j<cur.length;j++) {
+      let flag = true;
+
+      for (let [di,dj] of darr) {
+        let ni = i+di, nj=j+dj;
+        
+        if (arr[ni] == null || arr[ni][nj] == null) continue;
+
+      
+        if (arr[ni][nj] <= cur[j]) flag = false;
+      }
+
+      if (flag) {
+        let cnt = 1;
+
+        let q =[[i,j]];
+        set.add(i+','+j);
+        while (q.length) {
+          let nq = [];
+
+          for (let [a, b] of q) {
+            for (let [da, db] of darr) {
+              let na = a+da, nb=b+db;
+              
+              if (arr[na] == null || arr[na][nb] == null || arr[na][nb] == 9) continue;
+              
+              let str = na + ',' + nb;
+              if (set.has(str)) continue;
+              set.add(str);
+              nq.push([na,nb]);
+              cnt++;
+            }
+          }
+
+          q = nq;
+        }
+
+        a.push(cnt);
+      }
+    }
+
+  }
+  
+  a.sort((a,b)=>b-a);
+
+  return a[0]*a[1]*a[2];
+}
