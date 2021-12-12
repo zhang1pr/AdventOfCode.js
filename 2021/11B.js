d0410c48138697898320abebbc56393e7ecb05f4
Chao Zhang <czhang@axonify.com>
2021-12-11 19:17:57 -0500

    Add 2021 day11

---
+++ 2021/11B.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const ddarr = [[0, 1], [0, -1], [1, 0], [-1, 0], [-1, -1], [-1, 1], [1, -1], [1, 1]];
+const readnum = (a) => a.split('\n').map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => a.split(/\s+/).map(a => Number(a)));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let set = new Set();
+  let arr = readword(input).map(a => a.split('').map(a=>+a));
+  let t = 0;
+
+  while (true) {
+    t++; 
+    set = new Set();
+    
+    let narr = arr.map(a => a.slice());
+
+    for (let i=0;i<narr.length;i++) {
+      let row = narr[i];
+      for (let j=0;j<row.length;j++) {
+        let cur = row[j], curs = i +','+j;
+        if (set.has(curs)) continue;
+ 
+        cur++;
+        narr[i][j] = cur;
+        
+        if (cur > 9) {
+          let q = [[i,j]];
+          
+          while (q.length) {
+            let nq = [];
+
+
+            for (let [a,b] of q) {
+              narr[a][b] = 0; 
+              set.add(a+','+b);
+
+              for (let [di, dj] of ddarr) {
+                let ni=a+di, nj=b+dj, str = ni+','+nj;
+                if (arr[ni] == null || arr[ni][nj] == null) {
+                  continue;
+                }
+           
+                if (set.has(str)) continue;
+                
+                narr[ni][nj]++;
+                
+                if (narr[ni][nj] > 9) {
+                  narr[ni][nj] = 0;
+                  nq.push([ni, nj]);
+                }   
+              }
+            }
+
+            q = nq;
+          }   
+        }   
+      }
+    }
+
+    if (narr.every(a => a.every(b => b==0))) return t;
+
+    arr = narr;
+  }
+}
