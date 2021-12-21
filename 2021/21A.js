23e1f8b39911aecd8605632461b34772f5ae9465
Chao Zhang <czhang@axonify.com>
2021-12-21 16:17:34 -0500

    Add 2021 day21

---
+++ 2021/21A.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.split('\n').map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => a.split(/\s+/).map(a => Number(a)));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let res = 0;
+  let a1 = 0, a2 = 0, f = true, t = 1, time = 0;
+  let [t1, t2] = readword(input).map(a => +a.split(': ')[1]);
+
+  while (a1 < 1000 && a2 < 1000) {
+    let cur = 3;
+
+    if (f) {
+      while (cur) {
+        cur--;
+        t1 += t;
+        t1 = t1 % 10;
+        if (t1 == 0) t1 = 10;
+        time++;
+        t++;
+        if (t == 101) t = 1;
+      }
+      a1 += t1;
+    } else {
+      while (cur) {
+        cur--;
+        t2 += t;
+        t2 = t2 % 10;
+        if (t2 == 0) t2 = 10;
+
+        time++;
+        t++;
+        if (t == 101) t = 1;
+      }
+      a2 += t2;
+    }
+
+    f = !f;
+  }
+  
+  if (a1 >= 1000) res = a2; else res = a1;
+  return res * time;
+}
---
+++ 2021/21B.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.split('\n').map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => a.split(/\s+/).map(a => Number(a)));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let map = new Map();
+  let a1 = 0, a2 = 0;
+  let [t1, t2] = readword(input).map(a => +a.split(': ')[1]);
+  let dices = [1,2,3];
+  
+  function dfs(f, t1, t2, a1, a2) {
+    let str = [...arguments].join(',');
+
+    if (map.has(str)) return map.get(str);
+    let arr = [0, 0];
+
+    if (a1 >= 21 || a2 >= 21) {
+      if (a1 >= 21) arr = [1, 0];
+      else arr = [0, 1];
+    } else {
+      if (f) {
+        for (let a of dices) {
+          for (let b of dices) { 
+            for (let c of dices) {
+              let d = a + b + c;
+              let ct1 = (t1 + d) % 10;
+              ct1 = ct1 == 0 ? 10 : ct1;
+    
+              let [p1, p2] = dfs(!f, ct1, t2, a1 + ct1, a2);
+              arr[0] += p1;
+              arr[1] += p2;
+            }
+          }
+        }
+      } else {
+        for (let a of dices) {
+          for (let b of dices) { 
+            for (let c of dices) {
+              let d = a + b + c;
+              let ct2 = (t2 + d) % 10;
+              ct2 = ct2 == 0 ? 10 : ct2;
+
+              let [p1, p2] = dfs(!f, t1, ct2, a1, a2 + ct2);
+              arr[0] += p1;
+              arr[1] += p2;
+            }
+          }
+        }
+      }
+    }
+
+    map.set(str, arr);
+    return arr;
+  }
+
+  return Math.max(...dfs(true, t1, t2, a1, a2, 0));
+}
