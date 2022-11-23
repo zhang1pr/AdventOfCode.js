6e6c697edb1bc6761142268aab0f98e5b80839b2
Chao Zhang <czhang@axonify.com>
2022-11-22 21:58:32 -0500

    Add 2015 day25

---
+++ 2015/25A.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.split('\n').map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => a.split(/\s+/).map(a => Number(a)));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let res = 20151125, multi = 252533, MOD = 33554393;
+  let [R,C] = input.match(/\d+/g).map(a=>+a);
+  let order = R+C-1;
+  let sum = order * (order+1) / 2 - (order - C) - 1;
+  
+  for (let i=0; i<sum; i++) {
+    res = (res * multi) % MOD;
+  }
+
+  return res;
+}
