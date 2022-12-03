6e90ba385fdbae849b4e6c3379ba154a376ffcab
Chao Zhang <czhang@axonify.com>
2022-12-03 17:09:00 -0500

    Change 2015 day25

--- 2015/25A.js
+++ 2015/25A.js
@@ -7,7 +7,7 @@ const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
 
 function A(input) {
   let res = 20151125, multi = 252533, MOD = 33554393;
-  let [R,C] = input.match(/\d+/g).map(a=>+a);
+  let [R,C] = readnum(input)
   let order = R+C-1;
   let sum = order * (order+1) / 2 - (order - C) - 1;
   
