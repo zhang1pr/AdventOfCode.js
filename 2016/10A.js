107fee64ebd11bb4b8a9a56127eabe435149ced6
Chao Zhang <czhang@axonify.com>
2022-12-03 06:22:20 -0500

    Change 2016 day10

--- 2016/10A.js
+++ 2016/10A.js
@@ -1,74 +1,56 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
 function A(input) {
-  const map = new Map();
-  const instructions = [];
+  let map = new Map(), lmap = new Map(), hmap = new Map();
+  let arr = readnum2d(input);
 
-  input.split('\n').forEach(instruction => {
-    const parts = instruction.split(' ');
+  let q = [];
 
-    if (instruction.startsWith('value')) {
-      const bot = parseInt(parts[5], 10);
-      const chip = parseInt(parts[1], 10);
+  for (let nums of arr) {
+    if (nums.length == 2) {
+      let [val, id] = nums;
+      
+      if (!map.has(id)) 
+        map.set(id, []);
 
-      if (map.has(bot)) {
-        const anotherChip = map.get(bot);
-        map.set(bot, [anotherChip, chip]);
-      } else {
-        map.set(bot, chip);
-      }
+      map.get(id).push(val);
+
+      if (map.get(id).length == 2) 
+        q.push(id);
     } else {
-      let bot1 = parseInt(parts[6], 10);
-      let bot2 = parseInt(parts[11], 10);
-
-      if (parts[5] === 'output') {
-        bot1 = -bot1;
-      }
-
-      if (parts[10] === 'output') {
-        bot2 = -bot2;
-      }
-
-      instructions.push([parseInt(parts[1], 10), bot1, bot2]);
+      let [id, lid, hid] = nums;
+      lmap.set(id, lid);
+      hmap.set(id, hid);
     }
-  });
+  }
+  
+  while (q.length) {
+    let nq = [];
+    for (let id of q) {
+      let low = lmap.get(id), high = hmap.get(id);
+      let [lval, hval] = map.get(id).sort((a,b)=>a-b);
 
-  while (true) {
-    for (let instruction of instructions) {
-      if (instruction !== '' && map.has(instruction[0]) && map.get(instruction[0]).length === 2) {
-        const chips = map.get(instruction[0]);
-        const chipLow = chips[0] < chips[1] ? chips[0] : chips[1];
-        const chipHigh = chips[0] > chips[1] ? chips[0] : chips[1];
+      if (lval == 17 && hval == 61)
+        return 73;
 
-        const botLow = instruction[1];
-        const botHigh = instruction[2];
+      map.set(id, []);
 
-        if (map.has(botLow)) {
-          const anotherChip = map.get(botLow);
+      if (!map.has(low))
+        map.set(low, []);
+      map.get(low).push(lval); 
+      if (map.get(low).length == 2) nq.push(low)  
 
-          if ([chipLow, anotherChip].includes(61) && [chipLow, anotherChip].includes(17)) {
-            return botLow;
-          }
-
-          map.set(botLow, [anotherChip, chipLow]);
-        } else {
-          map.set(botLow, chipLow);
-        }
-
-        if (map.has(botHigh)) {
-          const anotherChip = map.get(botHigh);
-
-          if ([chipHigh, anotherChip].includes(61) && [chipHigh, anotherChip].includes(17)) {
-            return botHigh;
-          }
-
-          map.set(botHigh, [anotherChip, chipHigh]);
-        } else {
-          map.set(botHigh, chipHigh);
-        }
-
-        map.delete(instruction[0]);
-
-        instruction = '';
-      }
+      if (!map.has(high))
+        map.set(high, []);
+      map.get(high).push(hval);
+      if (map.get(high).length == 2) nq.push(high)  
     }
+
+    q = nq;  
   }
 }
