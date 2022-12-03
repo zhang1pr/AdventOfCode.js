a25e81afa34b9e88d5381bc6d81b49ba6ca12dd3
Chao Zhang <czhang@axonify.com>
2022-12-03 01:05:38 -0500

    Add 2022 day3

---
+++ 2022/3A.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.split('\n').map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => a.split(/\s+/).map(a => Number(a)));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let res = 0;
+  let arr = readword(input);
+
+  for (let str of arr) {
+    let l = str.length;
+    let a = str.slice(0, l/2), b = str.slice(l/2);
+
+    for (let ch of a) {
+      if (b.includes(ch)) {
+        if (ch >= 'a') res += ch.charCodeAt() - 97 + 1;
+        else res += ch.charCodeAt() - 65 + 27;
+        break;
+      }
+    }
+  }
+
+  return res;
+}
