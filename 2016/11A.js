e7fc7afb8ee39ae48daa8ba49cdd0035a5432e40
Chao Zhang <czhang@axonify.com>
2022-12-06 05:02:11 -0500

    Change 2016 day11

--- 2016/11A.js
+++ 2016/11A.js
@@ -1,54 +1,153 @@
-function B(input) {
-  const floors = [];
-  let steps = 0;
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
 
-  input.split('\n').forEach(line => {
-    let count = 0;
+function A(input) {
+  let map = new Map(), curId = 1;
+  let arr = readword(input).map(
+    a => (a.match(/[a-zA-Z-]* (generator|microchip)/g) || []).map(
+      a => {
+        let name = a.includes('-') ? a.split('-')[0] : a.split(' ')[0];
+        let sign = a.split(' ')[1] == 'generator' ? 1 : -1;
+        if (!map.has(name)) {
+          map.set(name, curId);
+          curId++;
+        }
 
-    const generatorRegex = /\w+ generator/g;
-    const microchipRegex = /\w+-\w+ microchip/g;
+        return map.get(name) * sign;
+      }
+    ).sort((a,b)=>a-b)
+  );
+  
+  let q = [[0, 0, arr]];
+  let set = new Set().add(toString(0, arr));
 
-    let generators = line.match(generatorRegex) || [];
-    let microchips = line.match(microchipRegex) || [];
+  while (q.length) {
+    let nq = [];
 
-    count += generators.length + microchips.length;
+    for (let [cur, steps, state] of q) {
+      let curFloor = state[cur];
+      let down = cur - 1, up = cur + 1;
+      let downFloor = state[down], upFloor = state[up];
 
-    floors.push(count);
-  });
+      if (down >= 0) {
+        for (let a of curFloor) {
+          let afterFloor = curFloor.filter(c => c != a);
 
-  const total = floors.reduce((prev, curr) => prev + curr);
-  let elevator = Math.min(floors[0], 2);
-  let floor = 0;
-  floors[0] -= elevator;
+          if (isCompatible(...downFloor, a) && isCompatible(...afterFloor)) {
+            let nsteps = steps + 1;
+            let nstate = state.map(a => a.slice());
+            nstate[down].push(a);
+            nstate[down].sort((a,b)=>a-b);
+            nstate[cur] = afterFloor;
 
-  while (floors[3] + 1 !== total) {
-    while (elevator < 2 && floor > 0) {
-      floor--;
-      const carry = Math.min(floors[floor], 2 - elevator);
+            let hash = toString(down, nstate);
 
-      if (carry > 0) {
-        elevator += carry;
-        floors[floor] -= carry;
+            if (!set.has(hash)) {
+              set.add(hash, nsteps);
+              nq.push([down, nsteps, nstate]);
+            }
+          }
+        }
       }
 
-      steps++;
-    }
+      if (up <= 3) {
+        let takeTwo = false;
+        
+        for (let i=0; i<curFloor.length; i++) {
+          for (let j=i+1; j<curFloor.length; j++) {
+            let a = curFloor[i], b = curFloor[j];
+            let afterFloor = curFloor.filter(c => c != a && c != b);
 
-    while (floor < 3) {
-      floor++;
-      const carry = Math.min(floors[floor], 2 - elevator);
+            if (isCompatible(...upFloor, a, b) && isCompatible(...afterFloor)) {
+              let nsteps = steps + 1;
+              let nstate = state.map(a => a.slice());
+              nstate[up].push(a);
+              nstate[up].push(b);
+              nstate[up].sort((a,b)=>a-b);
+              nstate[cur] = afterFloor;
 
-      if (carry > 0) {
-        elevator += carry;
-        floors[floor] -= carry;
+              if (done(nstate))
+                return nsteps;
+            
+              let hash = toString(up, nstate);
+              if (!set.has(hash)) {
+                takeTwo = true;
+                set.add(hash, nsteps);
+                nq.push([up, nsteps, nstate]);
+              }
+            }
+          }
+        }
+
+        if (!takeTwo) {
+          for (let a of curFloor) { 
+            let afterFloor = curFloor.filter(c => c != a);
+
+            if (isCompatible(...upFloor, a) && isCompatible(...afterFloor)) {
+              let nsteps = steps + 1;
+              let nstate = state.map(a => a.slice());
+              nstate[up].push(a);
+              nstate[up].sort((a,b)=>a-b);
+              nstate[cur] = afterFloor;
+
+              if (done(nstate)) {
+                return nsteps;
+              }
+
+              let hash = toString(up, nstate);
+
+              if (!set.has(hash)) {
+                set.add(hash, nsteps);
+                nq.push([up, nsteps, nstate]);
+              }
+            }
+          }
+        }
       }
-
-      steps++;
     }
 
-    floors[3] += 1;
-    elevator--;
-  }
+    q = nq;
+  }      
+}
 
-  return steps;
+function toString(floor, state) {
+  return floor + ':' + state.map(a => {
+    let pair = pos = neg = 0;
+    let set = new Set(a);
+
+    for (let x of set)
+      if (set.has(-x))
+        pair++;
+      else if (x > 0)
+        pos++;
+      else
+        neg++;
+
+    return [pair/2, pos, neg].join(',');
+  }).join(' ');
+}
+
+function done(state) {
+  for (let i=0; i<3; i++)
+    if (state[i].length > 0)
+      return false;
+  
+  return true;
+}
+
+function isCompatible(...items) {
+  let pos = new Set(items.filter(a => a > 0));
+  let neg = items.filter(a => a < 0);
+
+  if (pos.size == 0) return true;
+
+  for (let microchip of neg)
+    if (!pos.has(-microchip))
+      return false;
+
+  return true;
 }
--- 2016/11B.js
+++ 2016/11B.js
@@ -1,55 +1,160 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
 function B(input) {
-    const floors = [];
-    let steps = 0;
-
-    input.split('\n').forEach(line => {
-      let count = 0;
-
-      const generatorRegex = /\w+ generator/g;
-      const microchipRegex = /\w+-\w+ microchip/g;
-
-      let generators = line.match(generatorRegex) || [];
-      let microchips = line.match(microchipRegex) || [];
-
-      count += generators.length + microchips.length;
-
-      floors.push(count);
-    });
-
-    floors[0] += 4;
-    const total = floors.reduce((prev, curr) => prev + curr);
-    let elevator = Math.min(floors[0], 2);
-    let floor = 0;
-    floors[0] -= elevator;
-
-    while (floors[3] + 1 !== total) {
-      while (elevator < 2 && floor > 0) {
-        floor--;
-        const carry = Math.min(floors[floor], 2 - elevator);
-
-        if (carry > 0) {
-          elevator += carry;
-          floors[floor] -= carry;
+  let map = new Map(), curId = 1;
+  let arr = readword(input);
+  arr[0] += `
+    An elerium generator.
+    An elerium-compatible microchip.
+    A dilithium generator.
+    A dilithium-compatible microchip.
+  `;
+  arr = arr.map(
+    a => (a.match(/[a-zA-Z-]* (generator|microchip)/g) || []).map(
+      a => {
+        let name = a.includes('-') ? a.split('-')[0] : a.split(' ')[0];
+        let sign = a.split(' ')[1] == 'generator' ? 1 : -1;
+        if (!map.has(name)) {
+          map.set(name, curId);
+          curId *= 2;
         }
 
-        steps++;
+        return map.get(name) * sign;
+      }
+    ).sort((a,b)=>a-b)
+  );
+  
+  let q = [[0, 0, arr]];
+  let set = new Set().add(toString(0, arr));
+
+  while (q.length) {
+    let nq = [];
+
+    for (let [cur, steps, state] of q) {
+      let curFloor = state[cur];
+      let down = cur - 1, up = cur + 1;
+      let downFloor = state[down], upFloor = state[up];
+
+      if (down >= 0) {
+        for (let a of curFloor) {
+          let afterFloor = curFloor.filter(c => c != a);
+
+          if (isCompatible(...downFloor, a) && isCompatible(...afterFloor)) {
+            let nsteps = steps + 1;
+            let nstate = state.map(a => a.slice());
+            nstate[down].push(a);
+            nstate[down].sort((a,b)=>a-b);
+            nstate[cur] = afterFloor;
+
+            let hash = toString(down, nstate);
+
+            if (!set.has(hash)) {
+              set.add(hash, nsteps);
+              nq.push([down, nsteps, nstate]);
+            }
+          }
+        }
       }
 
-      while (floor < 3) {
-        floor++;
-        const carry = Math.min(floors[floor], 2 - elevator);
+      if (up <= 3) {
+        let takeTwo = false;
+        
+        for (let i=0; i<curFloor.length; i++) {
+          for (let j=i+1; j<curFloor.length; j++) {
+            let a = curFloor[i], b = curFloor[j];
+            let afterFloor = curFloor.filter(c => c != a && c != b);
 
-        if (carry > 0) {
-          elevator += carry;
-          floors[floor] -= carry;
+            if (isCompatible(...upFloor, a, b) && isCompatible(...afterFloor)) {
+              let nsteps = steps + 1;
+              let nstate = state.map(a => a.slice());
+              nstate[up].push(a);
+              nstate[up].push(b);
+              nstate[up].sort((a,b)=>a-b);
+              nstate[cur] = afterFloor;
+
+              if (done(nstate))
+                return nsteps;
+            
+              let hash = toString(up, nstate);
+              if (!set.has(hash)) {
+                takeTwo = true;
+                set.add(hash, nsteps);
+                nq.push([up, nsteps, nstate]);
+              }
+            }
+          }
         }
 
-        steps++;
-      }
+        if (!takeTwo) {
+          for (let a of curFloor) { 
+            let afterFloor = curFloor.filter(c => c != a);
 
-      floors[3] += 1;
-      elevator--;
+            if (isCompatible(...upFloor, a) && isCompatible(...afterFloor)) {
+              let nsteps = steps + 1;
+              let nstate = state.map(a => a.slice());
+              nstate[up].push(a);
+              nstate[up].sort((a,b)=>a-b);
+              nstate[cur] = afterFloor;
+
+              if (done(nstate)) {
+                return nsteps;
+              }
+
+              let hash = toString(up, nstate);
+
+              if (!set.has(hash)) {
+                set.add(hash, nsteps);
+                nq.push([up, nsteps, nstate]);
+              }
+            }
+          }
+        }
+      }
     }
 
-    return steps;
-  }
+    q = nq;
+  }      
+}
+
+function toString(floor, state) {
+  return floor + ':' + state.map(a => {
+    let pair = pos = neg = 0;
+    let set = new Set(a);
+
+    for (let x of set)
+      if (set.has(-x))
+        pair++;
+      else if (x > 0)
+        pos++;
+      else
+        neg++;
+
+    return [pair/2, pos, neg].join(',');
+  }).join(' ');
+}
+
+function done(state) {
+  for (let i=0; i<3; i++)
+    if (state[i].length > 0)
+      return false;
+  
+  return true;
+}
+
+function isCompatible(...items) {
+  let pos = new Set(items.filter(a => a > 0));
+  let neg = items.filter(a => a < 0);
+
+  if (pos.size == 0) return true;
+
+  for (let microchip of neg)
+    if (!pos.has(-microchip))
+      return false;
+
+  return true;
+}
