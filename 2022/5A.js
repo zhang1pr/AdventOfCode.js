ae7f6058c87e538461b05801cbcb8322b32f8312
Chao Zhang <czhang@axonify.com>
2022-12-06 04:59:36 -0500

    Add 2022 day5

---
+++ 2022/5A.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8');
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let [cargo, move] = input.split('\n\n');
+  
+  cargo = cargo.split('\n');
+  move = readnum2d(move.trim());
+
+  let stacks = [[]], idx;
+  for (let i=1; i<cargo[0].length; i+=4) {
+    for (let j=cargo.length-1; j>=0; j--) {
+      let item = cargo[j][i];
+
+      if ('1' <= item && item <= '9') {
+        idx = item;
+        stacks[idx] = [];
+      } else if ('A' <= item && item <= 'Z') {
+        stacks[idx].push(item);
+      } 
+    }
+  }
+  
+  for (let [num, from, to] of move) {
+    while (num) {
+      num--;
+      stacks[to].push(stacks[from].pop());
+    }
+  }
+
+  return stacks.map(a => a.at(-1) || '').join('');
+}
