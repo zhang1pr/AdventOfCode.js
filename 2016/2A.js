a99832f2d00332178569f9debaa3cf51634bbe57
Chao Zhang <czhang@axonify.com>
2022-12-06 14:26:50 -0500

    Change 2016 day2

--- 2016/2A.js
+++ 2016/2A.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync(0, 'utf8').trim();
 const dmap = new Map([['U', [-1, 0]],['D', [1, 0]],['L', [0, -1]],['R', [0, 1]]]);
+const isIn = (r,c,R,C) => 0 <= r && r < R && 0 <= c && c < C; 
 const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
 const readnum2d = (a) => a.split('\n').map(a => readnum(a));
 const readword = (a) => a.split('\n');
@@ -15,7 +16,7 @@ function A(input) {
     for (let ch of str) {
       let [dr, dc] = dmap.get(ch);
       
-      if (0<=r+dr&& r+dr<3 && 0<=c+dc && c+dc<3) {
+      if (isIn(r+dr, c+dc, 3, 3)) {
         r = r+dr;
         c = c+dc;
       }
@@ -27,3 +28,4 @@ function A(input) {
 
   return res;
 }
+console.log(A(input))
