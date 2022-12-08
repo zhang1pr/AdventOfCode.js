e95688884dc3eeb194b80cf5de51b76d3dae35e3
Chao Zhang <czhang@axonify.com>
2022-12-08 00:59:31 -0500

    Add 2022 day8

---
+++ 2022/8A.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const darr = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+const isIn = (r,c,R,C) => 0 <= r && r < R && 0 <= c && c < C; 
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let res = 0;
+  let arr = readword(input);
+  let R = arr.length, C = arr[0].length;
+
+  for (let r=0; r<R; r++) {
+    for (let c=0; c<C; c++) {
+      let flag = false;
+      
+      for (let [dr,dc] of darr) {
+        if (flag) break;
+
+        let nr=r+dr, nc=c+dc;
+        while (isIn(nr,nc,R,C) && arr[nr][nc] < arr[r][c]) {
+          nr+=dr, nc+=dc;
+        }
+
+        if (!isIn(nr,nc,R,C)) {
+          res++;
+          flag = true;
+        }
+      }
+    }
+  }
+
+  return res;
+}
