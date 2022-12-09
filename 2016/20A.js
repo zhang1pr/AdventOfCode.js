11081587890512a8ad1e68db9d0538ee09c63241
Chao Zhang <czhang@axonify.com>
2022-12-09 18:33:06 -0500

    Add 2016 day20

---
+++ 2016/20A.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let arr = readnum2d(input).sort((a,b)=>a[0] - b[0])
+
+  let last = 0;
+
+  for (let [start, end] of arr) {
+    if (last + 1 >= start) {
+      last = Math.max(last, end);
+    } else {
+      return last + 1;
+    }
+  }
+}
---
+++ 2016/20B.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let res = 0;
+  let arr = readnum2d(input).sort((a,b)=>a[0] - b[0]);
+  arr.push([4294967296, 4294967296])
+  let last = 0;
+
+  for (let [start, end] of arr) {
+    res += Math.max(0, start - last - 1);
+    last = Math.max(last, end);
+  }
+
+  return res;
+}
