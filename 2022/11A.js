90e75924aa11ca85040f5e118c56a465cb31f4e7
Chao Zhang <czhang@axonify.com>
2022-12-11 04:40:11 -0500

    Add 2022 day11

---
+++ 2022/11A.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let arr = readword(input);
+  let cnt = Array(arr.length).fill(0);
+  let q = [];
+
+  for (let i = 0; i < arr.length; i+=7) {
+    let [id] = readnum(arr[i]);
+    let items = readnum(arr[i+1]);
+    q[id] = items;
+  }
+
+  for (let rd = 1; rd <= 20; rd++) {
+    for (let i = 0; i < arr.length; i+=7) {
+      let [id] = readnum(arr[i]);
+      let op = arr[i+2].split('=')[1];
+      let [div] = readnum(arr[i+3]);
+      let [t] = readnum(arr[i+4]);
+      let [f] = readnum(arr[i+5]);
+      
+      for (let x of q[id]) {
+        let old = x;
+        let nx = Math.floor(eval(op) / 3);
+        cnt[id]++;
+
+        if (nx % div == 0)
+          q[t].push(nx);
+        else 
+          q[f].push(nx);
+      }
+
+      q[id] = [];
+    }
+  }
+
+  cnt.sort((a,b)=>b-a);
+  return cnt[0]*cnt[1];
+}
