8cb6fab3f631cee2b5991853366291419dc8d6a2
Chao Zhang <czhang@axonify.com>
2022-12-12 17:14:23 -0500

    Change 2017 day5

--- 2017/5A.js
+++ 2017/5A.js
@@ -1,12 +1,20 @@
-function A(input) {
-  const array = input.split('\n').map(Number);
-  let step = 0;
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/-?\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
 
-  for (let i = 0; i < array.length;) {
-    step += 1;
-    array[i] += 1;
-    i += array[i] - 1;
+function A(input) {
+  let res = 0;
+  let arr = readnum(input);
+
+  let i=0;
+  while (i < arr.length) {
+    arr[i]++;
+    i+=arr[i]-1;
+    res++;
   }
 
-  return step;
+  return res;
 }
--- 2017/5B.js
+++ 2017/5B.js
@@ -1,18 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/-?\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
 function B(input) {
-  const array = input.split('\n').map(Number);
-  let step = 0;
+  let res = 0;
+  let arr = readnum(input);
 
-  for (let i = 0; i < array.length;) {
-    step += 1;
-    const temp = array[i];
-    if (temp >= 3) {
-      array[i] -= 1;
-    } else {
-      array[i] += 1;
-    }
-
-    i += temp;
+  let i=0;
+  while (i < arr.length) {
+    let diff = arr[i] >= 3 ? -1 : 1;
+    arr[i]+=diff;
+    i+=arr[i]-diff;
+    res++;
   }
 
-  return step;
+  return res;
 }
