73c72b4a0b5f7c951f51748961576f9ce5d78781
Chao Zhang <czhang@axonify.com>
2022-12-13 03:16:48 -0500

    Add 2022 day13

---
+++ 2022/13A.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let res = 0;
+  let arr = readword(input);
+
+  function DFS(a,b) {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    } else if (Number.isInteger(a) ^ Number.isInteger(b)) {
+      if (Number.isInteger(a)) return DFS([a],b);
+      else return DFS(a,[b]);
+    }
+
+    let i=0, j=0;
+    while (i < a.length && j < b.length) {
+      let val = DFS(a[i], b[j]);
+
+      if (val != 0)
+        return val;
+
+      i++;
+      j++;
+    }
+
+    if (i < a.length) return 1;
+    if (j < b.length) return -1;
+    return 0;
+  }
+
+  for (let idx=0; idx<arr.length; idx+=3) {
+    t++;
+    let a = eval(arr[idx]), b = eval(arr[idx+1]);
+
+    if (DFS(a,b) < 0)
+      res += t;
+  }
+
+  return res;
+}
---
+++ 2022/13B.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let res = 1;
+  let arr = readword(input).filter(a=>Boolean(a));
+  let driver1 = '[[2]]', driver2 = '[[6]]';
+  arr.push(driver1);
+  arr.push(driver2);
+
+  function DFS(a,b) {
+    if (Number.isInteger(a) && Number.isInteger(b)) {
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    } else if (Number.isInteger(a) ^ Number.isInteger(b)) {
+      if (Number.isInteger(a)) return DFS([a],b);
+      else return DFS(a,[b]);
+    }
+
+    let i=0, j=0;
+    while (i < a.length && j < b.length) {
+      let val = DFS(a[i], b[j]);
+
+      if (val != 0)
+        return val;
+
+      i++;
+      j++;
+    }
+
+    if (i < a.length) return 1;
+    if (j < b.length) return -1;
+    return 0;
+  }
+
+  arr.sort((a,b) => DFS(eval(a), eval(b)));
+
+  for (let i=0; i<arr.length; i++)
+    if (arr[i] == driver1 || arr[i] == driver2)
+      res *= i+1;
+
+  return res;
+}
