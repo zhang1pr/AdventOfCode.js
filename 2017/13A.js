c38d262cdccad08a6d5a417dd3304d008c2d37ce
Chao Zhang <czhang@axonify.com>
2022-12-14 03:34:15 -0500

    Add 2017 day13

---
+++ 2017/13A.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let res = 0;
+  let arr = readnum2d(input);
+
+  for (let [depth, range] of arr) {
+    let pos = depth % (2 * range - 2);
+
+    if (pos == 0) {
+      res += depth * range;
+    }
+  }
+
+  return res;
+}
---
+++ 2017/13B.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let res = 0;
+  let arr = readnum2d(input);
+
+  function canBeCaught(delay) {  
+    for (let [depth, range] of arr) {
+      let pos = (depth + delay) % (2 * range - 2);
+  
+      if (pos == 0) 
+        return true;
+    }
+  
+    return false;
+  }
+
+  while (true) {
+    if (canBeCaught(res)) {
+      res++;
+    } else {
+      break;
+    }
+  }
+
+  return res;
+}
