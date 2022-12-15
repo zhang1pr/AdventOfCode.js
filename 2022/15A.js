989d8a4608821ad791f9609dd3894cf482ab436d
Chao Zhang <czhang@axonify.com>
2022-12-15 04:21:11 -0500

    Add 2022 day15

---
+++ 2022/15A.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/-?\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let set = new Set(), res = 0;
+  let arr = readnum2d(input);
+
+  const dist = (a,b,c,d) => Math.abs(a-c) + Math.abs(b-d);
+
+  let y = 2000000;
+  let intervals = [];
+
+  for (let [a,b,c,d] of arr) {
+    let closest = dist(a,b,c,d);
+    let dToY = dist(a,b,a,y);
+
+    if (closest >= dToY) {
+      let diff = closest-dToY;
+      intervals.push([a-diff,a+diff]);
+    }
+  }
+
+  intervals.sort((a,b)=>a[0]-b[0]);
+
+  let [lastStart, lastEnd] = intervals[0];
+  for (let [start,end] of intervals)
+    if (start > lastEnd)
+      res += lastEnd - lastStart + 1;
+    else
+      lastEnd = Math.max(lastEnd, end);
+  
+  res += lastEnd - lastStart + 1;
+
+  for (let [a,b,c,d] of arr)
+    if (!set.has(c) && d == y) {
+      set.add(c);
+      res--;
+    }
+
+  return res;
+}
---
+++ 2022/15B.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/-?\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let arr = readnum2d(input);
+
+  const dist = (a,b,c,d) => Math.abs(a-c) + Math.abs(b-d);
+
+  let MAX = 4000000;
+  for (let y=0; y<=MAX; y++) {
+    let intervals = [];
+
+    for (let [a,b,c,d] of arr) {
+      let closest = dist(a,b,c,d);
+      let dToY = dist(a,b,a,y);
+
+      if (closest >= dToY) {
+        let diff = closest-dToY;
+        intervals.push([a-diff,a+diff]);
+      }
+    }
+
+    intervals.push([MAX+1,MAX+1])
+    intervals.sort((a,b)=>a[0]-b[0]);
+    let last = 0;
+
+    for (let [start,end] of intervals)
+      if (start > last)
+        return last*4000000+y;
+      else 
+        last = Math.max(last,end+1);
+  }
+}
