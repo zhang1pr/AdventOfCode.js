d20bb88656ede675d7712629a5f3ac9964e3cd9e
Chao Zhang <czhang@axonify.com>
2022-12-17 16:21:05 -0500

    Add 2017 day16

---
+++ 2017/16B.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let N = 1000000000;
+  let arr = input.split(',');
+  let str = 'abcdefghijklmnop';
+
+  function process() {
+    for (let ins of arr) {
+      if (ins.startsWith('s')) {
+        let [idx] = readnum(ins);
+        str = str.slice(-idx) + str.slice(0, -idx);
+      } else if (ins.startsWith('x')) {
+        let [a,b] = readnum(ins);
+        if (a > b) 
+          [a,b] = [b,a];
+        str = str.slice(0,a) + str[b] + str.slice(a+1,b) + str[a] + str.slice(b+1);
+      } else if (ins.startsWith('p')) {
+        let charA = ins[1], charB = ins[3];
+        let [a,b] = [str.indexOf(charA), str.indexOf(charB)];
+  
+        if (a > b) 
+          [a,b] = [b,a];
+        str = str.slice(0,a) + str[b] + str.slice(a+1,b) + str[a] + str.slice(b+1);
+      } 
+    }
+  }
+
+  do {
+    t++;
+    process();
+  } while (str != 'abcdefghijklmnop')
+
+  t = N % t;
+  while (t) {
+    t--;
+    process();
+  }
+
+  return str;
+}
