99067262f2daa2ce6d9085e26bff7308fe75f7a0
Chao Zhang <czhang@axonify.com>
2022-12-17 17:30:54 -0500

    Add 2017 day17

---
+++ 2017/17A.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let steps = +input;
+
+  let arr = [0];
+  let idx = 0;
+
+  for (let num=1; num<=2017; num++) {
+    idx = (idx + steps) % arr.length;
+
+    arr.splice(idx+1, 0, num);
+    idx++;
+  }
+
+  return arr[(idx+1)%arr.length];
+}
---
+++ 2017/17B.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let steps = +input;
+
+  let len = 1;
+  let idx = 0, res;
+  for (let num=1; num<=50000000; num++) {
+    idx = (idx + steps) % len;
+    idx++;
+    len++;
+
+    if (idx == 1)
+      res = num;
+  }
+
+  return res;
+}
