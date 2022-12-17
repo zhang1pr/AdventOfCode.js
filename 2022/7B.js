60fb3bd3ea7b216cac24fee4acc95376b14feaf7
Chao Zhang <czhang@axonify.com>
2022-12-17 16:24:13 -0500

    Change 2022 day7

--- 2022/7B.js
+++ 2022/7B.js
@@ -5,7 +5,7 @@ const readnum2d = (a) => a.split('\n').map(a => readnum(a));
 const readword = (a) => a.split('\n');
 const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
 
-function A(input) {
+function B(input) {
   let res = Infinity;
   let arr = readword2d(input);
   let root = new Map();
