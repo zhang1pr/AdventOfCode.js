4caf721a351feaef85800a4287bea9ae4f59ba1a
Chao Zhang <czhang@axonify.com>
2022-12-26 02:06:17 -0500

    Add 2017 day19

---
+++ 2017/19B.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8');
+const dmap = new Map([['v', [1, 0]],['>', [0,1]],['^', [-1, 0]],['<', [0,-1]]]);
+const isIn = (r,c,R,C) => 0 <= r && r < R && 0 <= c && c < C; 
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let res = 0;
+  let arr = readword(input);
+  let dstr = '^>v<';
+  let R = arr.length, C = arr[0].length;
+
+  let r=0, c=arr[0].indexOf('|'), dir = 'v';
+
+  while (true) {
+    let ch = arr[r][c];
+    if (ch == ' ') break;
+    res++;
+
+    if (ch == '+') {
+      let idx = dstr.indexOf(dir);
+      let cand = [idx+1,idx+3];
+
+      for (let idx of cand) {
+        let ndir = dstr[idx % 4];
+        let [dr,dc] = dmap.get(ndir);
+        let nr=r+dr,nc=c+dc;
+
+        if (isIn(nr,nc,R,C) && arr[nr][nc] != ' ' && arr[nr][nc] != null) {
+          r=nr, c=nc;
+          dir = ndir;
+          break;
+        }
+      }
+    } else {
+      let [dr,dc] = dmap.get(dir);
+      r+=dr, c+=dc;
+    }
+  }
+
+  return res;
+}
