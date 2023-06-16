e72da17f27fd27be150952653cece9a2aa97bce8
Chao Zhang <czhang@axonify.com>
2023-06-15 20:29:15 -0400

    Add 2017 day23

---
+++ 2017/23A.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let map = new Map(), res = 0;
+  let arr = readword2d(input);
+
+  for (let i=0; i<arr.length; ) {
+    let [ins, a, b] = arr[i];
+    let va = a, vb = b;
+
+    if (a == +a) va = +a; 
+    else va = map.get(a) || 0; 
+
+    if (b == +b) vb = +b; 
+    else vb = map.get(b) || 0;
+
+    if (ins == 'set') {
+      map.set(a, vb);
+      i++;
+    } else if (ins == 'sub') {
+      map.set(a, va - vb);
+      i++;
+    } else if (ins == 'mul') {
+      map.set(a, va * vb);
+      res++;
+      i++;
+    } else if (ins == 'jnz') {
+      if (va != 0)
+        i += vb;
+      else 
+        i++;
+    }
+  }
+  
+  return res;
+}
---
+++ 2017/23B.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let res = 0;
+  let arr = readword2d(input);
+
+  let start, max, interval, vd;
+
+  for (let i=0; i<arr.length; i++) {
+    let [ins, a, b] = arr[i];
+    
+    if (ins == 'set' && a == 'b') start = +b;
+    if (ins == 'mul' && a == 'b') start *= +b;
+    if (ins == 'sub' && a == 'b') start += -b;
+    if (ins == 'sub' && a == 'c') max = -b;
+    if (ins == 'sub' && a == 'b') interval = -b;
+    if (ins == 'set' && a == 'd') vd = +b;
+  }
+
+  for (let cur = start; cur <= start + max; cur += interval) {
+    let d = vd;
+
+    while (cur % d != 0) d++;
+
+    if (cur !== d) res++;
+  }
+
+  return res;
+}
