99ad9f6cf0b42faf6d657733072228fb6fbb7fe3
Chao Zhang <czhang@axonify.com>
2023-07-01 17:16:48 -0400

    Add 2018 day14

---
+++ 2018/14A.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function A(input) {
+  let arr = [3,7];
+  let len = +input;
+  let first = 0, second = 1;
+
+  while (arr.length < len + 10) {
+    let a = arr[first], b = arr[second]; 
+    let digits = arr[first] + arr[second]; 
+    
+    for (let x of digits.toString())
+      arr.push(+x);
+
+    first = (first + a + 1) % arr.length;
+    second = (second + b + 1) % arr.length;
+  }
+
+  return arr.slice(len, len+10).join('');
+}
---
+++ 2018/14B.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function B(input) {
+  let arr = [3,7];
+  let first = 0, second = 1;
+
+  while (!arr.slice(-input.length-1).join('').includes(input)) {
+    let a = arr[first], b = arr[second]; 
+    let digits = arr[first] + arr[second]; 
+
+    for (let x of digits.toString())
+      arr.push(+x);
+
+    first = (first + a + 1) % arr.length;
+    second = (second + b + 1) % arr.length;
+  }
+
+  return arr.length - input.length - 1 + arr.slice(-input.length-1).join('').indexOf(input);
+}
