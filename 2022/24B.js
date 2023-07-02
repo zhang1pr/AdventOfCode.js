d06e08a45f8a6ceb7cac94283c163a27d4f11428
Chao Zhang <czhang@axonify.com>
2023-07-02 19:08:01 -0400

    Change template

--- 2022/24B.js
+++ 2022/24B.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0, 'utf8').trim();
 const dmap = new Map([['v', [1, 0]],['>', [0,1]],['^', [-1, 0]],['<', [0,-1]]]);
 const darr = [[0, 0], [0, 1], [0, -1], [1, 0], [-1, 0]];
 const isIn = (r,c,R,C) => 0 <= r && r < R && 0 <= c && c < C;
-const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
+const readnum = (a) => (a.match(/\d+/g) || []).map(a => Number(a));
 const readnum2d = (a) => a.split('\n').map(a => readnum(a));
 const readword = (a) => a.split('\n');
 const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
