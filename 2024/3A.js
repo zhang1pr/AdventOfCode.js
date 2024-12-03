190662ffcfc2a7b25e577768ee9df555d6ebb105
Chao Zhang <czhang@axonify.com>
2024-12-03 02:59:38 -0500

    Add 2024 day3

---
+++ 2024/3A.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => (a.match(/\d+/g) || []).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function solve(input) {
+  let res = 0;
+  let matches = input.matchAll(/mul[(](\d+),(\d+)[)]/g);
+
+  for (let [match, a, b] of matches)
+    res += a * b;
+
+  return res;
+}
+
+console.log(solve(input));
---
+++ 2024/3B.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => (a.match(/\d+/g) || []).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function solve(input) {
+  let res = 0;
+  let matches = input.matchAll(/mul[(](\d+),(\d+)[)]|do[(][)]|don't[(][)]/g);
+  let f = true;
+
+  for (let [match, a, b] of matches) {
+    if (match == 'do()')
+      f = true;
+    else if (match == 'don\'t()')
+      f = false;
+    else if (f)
+      res += a * b;
+  }
+
+  return res;
+}
+
+console.log(solve(input));
