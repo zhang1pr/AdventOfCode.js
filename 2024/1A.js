87f7fb6c96f403f67931c8e863f7ff2896938686
Chao Zhang <czhang@axonify.com>
2024-12-03 02:24:42 -0500

    Add 2024 day1

---
+++ 2024/1A.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => (a.match(/\d+/g) || []).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
+
+function solve(input) {
+  let res = 0;
+  let arr = readnum2d(input);
+  let A = [], B = [];
+
+  for (let [a, b] of arr)
+    A.push(a), B.push(b);
+
+  A.sort((a, b) => a - b);
+  B.sort((a, b) => a - b);
+
+  for (let i = 0; i < A.length; i++)
+    res += Math.abs(A[i] - B[i]);
+
+  return res;
+}
+
+console.log(solve(input));
