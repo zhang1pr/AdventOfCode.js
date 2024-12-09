2efb68be3b14cf01f857b845eb913f3cc126c6ff
Chao Zhang <czhang@axonify.com>
2024-12-08 23:13:10 -0500

    Change 2019 day2

--- 2019/2A.js
+++ 2019/2A.js
@@ -1,15 +1,32 @@
-function A(input) {
-  input = input.split(',').map(i => parseInt(i));
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => (a.match(/\d+/g) || []).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
 
-  for (let i = 0; i < input.length; i = i + 4) {
-    if (input[i] === 99) {
-      return input[0];
+function solve(input) {
+  let arr = input.split(',').map(a => +a);
+  let idx = 0;
+  arr[1] = 12, arr[2] = 2;
+
+  while (true) {
+    let [op, a, b, c] = arr.slice(idx, idx + 4);
+    let val;
+
+    if (op == 99) {
+      break;
+    } else if (op == 1) {
+      val = arr[a] + arr[b];
+    } else if (op == 2) {
+      val = arr[a] * arr[b];
     }
 
-    if (input[i] === 1) {
-      input[input[i+3]] = input[input[i+1]] + input[input[i+2]];
-    } else if (input[i] === 2) {
-      input[input[i+3]] = input[input[i+1]] * input[input[i+2]];
-    }
+    arr[c] = val;
+    idx += 4;
   }
+
+  return arr[0];
 }
+
+console.log(solve(input));
--- 2019/2B.js
+++ 2019/2B.js
@@ -1,27 +1,40 @@
-function B(input) {
-  input = input.split(',').map(i => parseInt(i));
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => (a.match(/\d+/g) || []).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
 
-  for (let a=0; a < 100; a++) {
-    for (let b=0; b < 100; b++) {
-      const inputCopy = input.slice();
-      inputCopy[1] = a;
-      inputCopy[2] = b;
+function solve(input) {
+  let arr = input.split(',').map(a => +a);
 
-      for (let i = 0; i < inputCopy.length; i = i+4) {
-        if (inputCopy[i] === 99) {
-          if (inputCopy[0] === 19690720) {
-            return a*100 + b;
-          } else {
-            break;
-          }
-        }
-
-        if (inputCopy[i] === 1) {
-          inputCopy[inputCopy[i+3]] = inputCopy[inputCopy[i+1]] + inputCopy[inputCopy[i+2]];
-        } else if (inputCopy[i] === 2) {
-          inputCopy[inputCopy[i+3]] = inputCopy[inputCopy[i+1]] * inputCopy[inputCopy[i+2]];
-        }
-      }
-    }
-  }
+  for (let i = 0; i < 100; i++)
+    for (let j = 0; j < 100; j++)
+      if (run(arr.slice(), i, j) == 19690720)
+        return 100 * i + j;
 }
+
+function run(arr, noun, verb) {
+  let idx = 0;
+  arr[1] = noun, arr[2] = verb;
+
+  while (true) {
+    let [op, a, b, c] = arr.slice(idx, idx + 4);
+    let val;
+
+    if (op == 99) {
+      break;
+    } else if (op == 1) {
+      val = arr[a] + arr[b];
+    } else if (op == 2) {
+      val = arr[a] * arr[b];
+    }
+
+    arr[c] = val;
+    idx += 4;
+  }
+
+  return arr[0];
+}
+
+console.log(solve(input));
