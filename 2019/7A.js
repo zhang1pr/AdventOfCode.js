4d985d4f85956722f4429219986f3b52f12be94a
Chao Zhang <czhang@axonify.com>
2024-12-14 06:03:51 -0500

    Change 2019 day7

--- 2019/7A.js
+++ 2019/7A.js
@@ -1,137 +1,78 @@
-function A(input) {
-  const instructions = [];
-  let max = 0;
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => (a.match(/\d+/g) || []).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
 
-  input.split(',').forEach(instruction => {
-    if (instruction != null) {
-      instructions.push(instruction);
-    }
-  });
+function solve(input) {
+  let res = 0;
+  let arr = input.split(',').map(a => +a);
 
-  const array = getPermutation([0,1,2,3,4]);
+  for (let x = 1234; x <= 43210; x++) {
+    let str = x.toString().padStart(5, '0');
 
-  for (method of array) {
-    let value = 0;
-    for (element of method) {
-      value = From2019Day5B(instructions.slice(), [element, value]);
-    }
+    if (new Set(str).size != 5)
+      continue;
 
-    max = Math.max(max, value);
+    let output = 0;
+
+    for (let ch of str)
+      if (ch > '4')
+        output = -1;
+
+    if (output == -1) continue;
+
+    for (let ch of str)
+      output = intcode(arr.slice(), [+ch, output]);
+
+
+    res = Math.max(res, output);
   }
 
-  return max;
+  return res;
 }
 
-function From2019Day5B(instructions, value) {
-  let position = 2;
-  let target;
+function intcode(arr, input) {
+  let idx = 0;
 
-  instructions[instructions[1]] = (value[0]).toString();
+  while (true) {
+    let [code, a, b, c] = arr.slice(idx, idx + 4);
 
-  while(true) {
-    const opcode = instructions[position];
-    let mode;
+    let op = code % 100;
+    code = (code - op) / 100;
+    let modeA = code % 10;
+    code = (code - modeA) / 10;
+    let modeB = code % 10;
 
-    if (opcode.length > 1) {
-      mode = opcode.slice(opcode.length-2);
-    } else {
-      mode = opcode;
-    }
+    let valA = modeA == 1 ? a : arr[a];
+    let valB = modeB == 1 ? b : arr[b];
 
-    if (mode === '99') {
-      return parseInt(target, 10);
-    } else {
-      mode = mode[mode.length-1];
-    }
-
-    if (mode === '4') {
-      if (instructions[position].length === 3 && instructions[position][0] === '1') {
-        target = instructions[position+1];
-      } else {
-        target = instructions[instructions[position+1]];
-      }
-
-      position += 2;
-    } else if (mode === '3') {
-      if (instructions[position].length === 3 && instructions[position][0] === '1') {
-        instructions[position] = (value[1]).toString();
-      } else {
-        instructions[instructions[position+1]] = (value[1]).toString();
-      }
-
-      position += 2;
-    } else {
-      let number1;
-      let number2;
-
-      if (instructions[position].length <= 2) {
-        number1 = instructions[instructions[position+1]];
-        number2 = instructions[instructions[position+2]];
-      } else if (instructions[position].length === 3) {
-        number1 = instructions[position][0] === '1' ? instructions[position+1] : instructions[instructions[position+1]];
-        number2 = instructions[instructions[position+2]];
-      } else {
-        number1 = instructions[position][1] === '1' ? instructions[position+1] : instructions[instructions[position+1]];
-        number2 = instructions[position][0] === '1' ? instructions[position+2] : instructions[instructions[position+2]];
-      }
-
-      if (mode === '5') {
-        if (number1 === '0') {
-          position += 3;
-        } else {
-          position = parseInt(number2, 10);
-        }
-
-        continue;
-      } else if (mode === '6') {
-        if (number1 !== '0') {
-          position += 3;
-        } else {
-          position = parseInt(number2, 10);
-        }
-
-        continue;
-      }
-
-      if (mode === '7') {
-        if (parseInt(number1, 10) >= parseInt(number2, 10)) {
-          instructions[instructions[position+3]] = '0';
-        } else {
-          instructions[instructions[position+3]] = '1';
-        }
-      } else if (mode === '8') {
-        if (number1 !== number2) {
-          instructions[instructions[position+3]] = '0';
-        } else {
-          instructions[instructions[position+3]] = '1';
-        }
-      } else if (mode === '1') {
-        instructions[instructions[position+3]] = (parseInt(number1, 10) + parseInt(number2, 10)).toString();
-      } else if (mode === '2') {
-        instructions[instructions[position+3]] = (parseInt(number1, 10) * parseInt(number2, 10)).toString();
-      }
-
-      position += 4;
+    if (op == 99) {
+      break;
+    } else if (op == 1) {
+      arr[c] = valA + valB;
+      idx += 4;
+    } else if (op == 2) {
+      arr[c] = valA * valB;
+      idx += 4;
+    } else if (op == 3) {
+      arr[a] = input.shift();
+      idx += 2;
+    } else if (op == 4) {
+      return valA;
+    } else if (op == 5) {
+      idx = valA != 0 ? valB : idx + 3;
+    } else if (op == 6) {
+      idx = valA == 0 ? valB : idx + 3;
+    } else if (op == 7) {
+      arr[c] = valA < valB ? 1 : 0;
+      idx += 4;
+    } else if (op == 8) {
+      arr[c] = valA == valB ? 1 : 0;
+      idx += 4;
     }
   }
 }
 
-function getPermutation(target) {
-  let result = [];
-
-  const permute = (array, progress = []) => {
-    if (array.length === 0) {
-      result.push(progress);
-    } else {
-      for (let i = 0; i < array.length; i++) {
-        let curr = array.slice();
-        let next = curr.splice(i, 1);
-        permute(curr.slice(), progress.concat(next));
-      }
-    }
-  }
-
-  permute(target);
-
-  return result;
-}
+console.log(solve(input));
--- 2019/7B.js
+++ 2019/7B.js
@@ -1,177 +1,96 @@
-function B(input) {
-  const instructions = [];
-  let max = 0;
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => (a.match(/\d+/g) || []).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
 
-  input.split(',').forEach(instruction => {
-    if (instruction != null) {
-      instructions.push(instruction);
-    }
-  });
+function solve(input) {
+  let res = 0;
+  let arr = input.split(',').map(a => +a);
 
-  const array = getPermutation([9,8,7,6,5]);
+  for (let x = 56789; x <= 98765; x++) {
+    let str = x.toString().padStart(5, '0');
 
-  for (method of array) {
-    let value = 0;
-    let flag = true;
-    let container = Array(5).fill(0).map(() => [instructions.slice(), 0]);
-    let initialFlag = true;
+    if (new Set(str).size != 5)
+      continue;
 
-    while (flag) {
-      for (let i = 0; i < method.length; i++) {
-        if (initialFlag) {
-          result = From2019Day5B(container[i][0], container[i][1], [method[i], value]);
-        } else {
-          result = From2019Day5B(container[i][0], container[i][1], value);
-        }
+    let output = 0;
 
-        if (Array.isArray(result) && result.length === 3) {
-          value = result[0];
-          container[i][1] = result[1];
-          container[i][0] = result[2];
-        } else {
-          if (Array.isArray(result)) {
-            value = result[1];
-          } else {
-            value = result;
-          }
+    for (let ch of str)
+      if (ch < '5')
+        output = -1;
 
-          if (i === 4) {
-            flag = false;
-            break;
-          }
-        }
+    if (output == -1) continue;
+
+    let amps = [...Array(5)].map(() => arr.slice());
+    for (let amp of amps)
+      amp.push(0);
+
+    let f = true;
+
+    while (true) {
+      for (let i = 0; i < str.length; i++) {
+        let input = f ? [+str[i], output] : [output, output];
+        output = intcode(amps[i], input);
+
+        if (output == null) break;
       }
-      initialFlag = false;
-    }
 
-    max = Math.max(max, value);
+      if (output == null) break;
+
+      f = false;
+
+      res = Math.max(res, output);
+    }
   }
 
-  return max;
+  return res;
 }
 
-function From2019Day5B(instructions, position, value) {
-  let target;
+function intcode(arr, input) {
+  let idx = arr.at(-1);
 
-  while(true) {
-    const opcode = instructions[position];
-    let mode;
+  while (true) {
+    let [code, a, b, c] = arr.slice(idx, idx + 4);
 
-    if (opcode.length > 1) {
-      mode = opcode.slice(opcode.length-2);
-    } else {
-      mode = opcode;
-    }
+    let op = code % 100;
+    code = (code - op) / 100;
+    let modeA = code % 10;
+    code = (code - modeA) / 10;
+    let modeB = code % 10;
 
-    if (mode === '99') {
-      return value;
-    } else {
-      mode = mode[mode.length-1];
-    }
+    let valA = modeA == 1 ? a : arr[a];
+    let valB = modeB == 1 ? b : arr[b];
 
-    if (mode === '4') {
-      if (instructions[position].length === 3 && instructions[position][0] === '1') {
+    if (op == 99) {
+      break;
+    } else if (op == 1) {
+      arr[c] = valA + valB;
+      idx += 4;
+    } else if (op == 2) {
+      arr[c] = valA * valB;
+      idx += 4;
+    } else if (op == 3) {
+      arr[a] = input.shift();
+      idx += 2;
+    } else if (op == 4) {
+      idx += 2;
+      arr[arr.length - 1] = idx;
 
-        target = instructions[position+1];
-        position += 2;
-        return [parseInt(target,10), position, instructions];
-      } else {
-        target = instructions[instructions[position+1]];
-        position += 2;
-        return [parseInt(target,10), position, instructions];
-      }
-
-
-    } else if (mode === '3') {
-      if (Array.isArray(value)) {
-        if (instructions[position].length === 3 && instructions[position][0] === '1') {
-          instructions[position] = (value[0]).toString();
-        } else {
-          instructions[instructions[position+1]] = (value[0]).toString();
-        }
-
-        value = value[1];
-      } else {
-        if (instructions[position].length === 3 && instructions[position][0] === '1') {
-          instructions[position] = (value).toString();
-        } else {
-          instructions[instructions[position+1]] = (value).toString();
-        }
-      }
-
-      position += 2;
-    } else {
-      let number1;
-      let number2;
-
-      if (instructions[position].length <= 2) {
-        number1 = instructions[instructions[position+1]];
-        number2 = instructions[instructions[position+2]];
-      } else if (instructions[position].length === 3) {
-        number1 = instructions[position][0] === '1' ? instructions[position+1] : instructions[instructions[position+1]];
-        number2 = instructions[instructions[position+2]];
-      } else {
-        number1 = instructions[position][1] === '1' ? instructions[position+1] : instructions[instructions[position+1]];
-        number2 = instructions[position][0] === '1' ? instructions[position+2] : instructions[instructions[position+2]];
-      }
-
-      if (mode === '5') {
-        if (number1 === '0') {
-          position += 3;
-        } else {
-          position = parseInt(number2, 10);
-        }
-
-        continue;
-      } else if (mode === '6') {
-        if (number1 !== '0') {
-          position += 3;
-        } else {
-          position = parseInt(number2, 10);
-        }
-
-        continue;
-      }
-
-      if (mode === '7') {
-        if (parseInt(number1,10) >= parseInt(number2,10)) {
-          instructions[instructions[position+3]] = '0';
-        } else {
-          instructions[instructions[position+3]] = '1';
-        }
-      } else if (mode === '8') {
-        if (number1 !== number2) {
-          instructions[instructions[position+3]] = '0';
-        } else {
-          instructions[instructions[position+3]] = '1';
-        }
-      } else if (mode === '1') {
-        instructions[instructions[position+3]] = (parseInt(number1, 10) + parseInt(number2, 10)).toString();
-      } else if (mode === '2') {
-        instructions[instructions[position+3]] = (parseInt(number1, 10) * parseInt(number2, 10)).toString();
-      }
-
-      position += 4;
+      return valA;
+    } else if (op == 5) {
+      idx = valA != 0 ? valB : idx + 3;
+    } else if (op == 6) {
+      idx = valA == 0 ? valB : idx + 3;
+    } else if (op == 7) {
+      arr[c] = valA < valB ? 1 : 0;
+      idx += 4;
+    } else if (op == 8) {
+      arr[c] = valA == valB ? 1 : 0;
+      idx += 4;
     }
   }
 }
 
-function getPermutation(target) {
-  let result = [];
-
-  const permute = (array, progress = []) => {
-    if (array.length === 0) {
-      result.push(progress);
-    } else {
-      for (let i = 0; i < array.length; i++) {
-        let curr = array.slice();
-        let next = curr.splice(i, 1);
-        permute(curr.slice(), progress.concat(next));
-      }
-    }
-  }
-
-  permute(target);
-
-  return result;
-}
+console.log(solve(input));
