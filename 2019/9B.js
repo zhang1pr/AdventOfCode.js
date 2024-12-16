f46189b6369fb3779b457b99f72b8816cf6d1019
Chao Zhang <czhang@axonify.com>
2024-12-16 03:13:02 -0500

    Change 2019 day9

--- 2019/9B.js
+++ 2019/9B.js
@@ -1,150 +1,73 @@
-function B(input) {
-  const instructions = [];
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+const readnum = (a) => (a.match(/\d+/g) || []).map(a => Number(a));
+const readnum2d = (a) => a.split('\n').map(a => readnum(a));
+const readword = (a) => a.split('\n');
+const readword2d = (a) => a.split('\n').map(a => a.split(/\s+/));
 
-  input.split(',').forEach(instruction => {
-    instructions.push(instruction);
-  });
+function solve(input) {
+  return intcode(input.split(',').map(a => +a));
+}
 
-  positionObject = {
-    1: 4,
-    2: 4,
-    3: 2,
-    4: 2,
-    7: 4,
-    8: 4,
-    9: 2
-  }
+function intcode(arr) {
+  let idx = arr.idx || 0;
+  let re = arr.re || 0;
 
-  let relativeBase = 0;
-  let position = 0;
-  let value = 2;
+  while (true) {
+    let [code, a, b, c] = arr.slice(idx, idx + 4);
 
-  positionObject = {
-    1: 4,
-    2: 4,
-    3: 2,
-    4: 2,
-    7: 4,
-    8: 4,
-    9: 2
-  }
+    let op = code % 100;
+    code = (code - op) / 100;
+    let modeA = code % 10;
+    code = (code - modeA) / 10;
+    let modeB = code % 10;
+    code = (code - modeB) / 10;
+    let modeC = code % 10;
 
-  while(true) {
-    const opcode = instructions[position];
-    let mode;
+    if (modeA == 2) a += re;
+    if (modeB == 2) b += re;
+    if (modeC == 2) c += re;
 
-    if (opcode.length > 1) {
-      mode = opcode.slice(opcode.length-2);
-    } else {
-      mode = opcode;
+    let valA = modeA == 1 ? a : arr[a];
+    let valB = modeB == 1 ? b : arr[b];
+    valA = valA || 0;
+    valB = valB || 0;
+
+    if (op == 99) {
+      arr.idx = idx;
+      arr.re = re;
+
+      break;
+    } else if (op == 1) {
+      arr[c] = valA + valB;
+      idx += 4;
+    } else if (op == 2) {
+      arr[c] = valA * valB;
+      idx += 4;
+    } else if (op == 3) {
+      arr[a] = 2;
+      idx += 2;
+    } else if (op == 4) {
+      idx += 2;
+      arr.idx = idx;
+      arr.re = re;
+
+      return valA;
+    } else if (op == 5) {
+      idx = valA != 0 ? valB : idx + 3;
+    } else if (op == 6) {
+      idx = valA == 0 ? valB : idx + 3;
+    } else if (op == 7) {
+      arr[c] = valA < valB ? 1 : 0;
+      idx += 4;
+    } else if (op == 8) {
+      arr[c] = valA == valB ? 1 : 0;
+      idx += 4;
+    } else if (op == 9) {
+      re += valA;
+      idx += 2;
     }
-
-    if (mode === '99') {
-      return;
-    } else {
-      mode = mode[mode.length-1];
-    }
-
-    let number1;
-    let number2;
-    let number3;
-    if (instructions[position].length <= 2) {
-      number1 = instructions[position+1];
-      number2 = instructions[position+2];
-      number3 = instructions[position+3];
-    } else if (instructions[position].length === 3) {
-      if (instructions[position][0] === '1') {
-        number1 = position+1;
-      } else if (instructions[position][0] === '2') {
-        number1 = parseInt(instructions[position+1], 10) + relativeBase;
-      } else {
-        number1 = instructions[position+1];
-      }
-
-      number2 = instructions[position+2];
-      number3 = instructions[position+3];
-    } else if (instructions[position].length === 4){
-      if (instructions[position][1] === '1') {
-        number1 = position+1;
-      } else if (instructions[position][1] === '2') {
-        number1 = parseInt(instructions[position+1], 10) + relativeBase;
-      } else {
-        number1 = instructions[position+1];
-      }
-
-      if (instructions[position][0] === '1') {
-        number2 = position+2;
-      } else if (instructions[position][0] === '2') {
-        number2 = parseInt(instructions[position+2], 10) + relativeBase;
-      } else {
-        number2 = instructions[position+2];
-      }
-
-      number3 = instructions[position+3];
-    } else if (instructions[position].length === 5){
-      if (instructions[position][2] === '1') {
-        number1 = position+1;
-      } else if (instructions[position][2] === '2') {
-        number1 = parseInt(instructions[position+1], 10) + relativeBase;
-      } else {
-        number1 = instructions[position+1];
-      }
-
-      if (instructions[position][1] === '1') {
-        number2 = position+2;
-      } else if (instructions[position][1] === '2') {
-        number2 = parseInt(instructions[position+2], 10) + relativeBase;
-      } else {
-        number2 = instructions[position+2];
-      }
-
-      if (instructions[position][0] === '1') {
-        number3 = position+3;
-      } else if (instructions[position][0] === '2') {
-        number3 = parseInt(instructions[position+3], 10) + relativeBase;
-      } else {
-        number3 = instruction[position+3];
-      }
-    }
-
-    if (mode === '9') {
-      relativeBase += parseInt(instructions[number1], 10);
-    } else if (mode === '4') {
-      return parseInt(instructions[number1], 10);
-    } else if (mode === '3') {
-      instructions[number1] = value.toString();
-    } else if (mode === '5') {
-      if (instructions[number1] === '0') {
-        position += 3;
-      } else {
-        position = parseInt(instructions[number2], 10);
-      }
-      continue;
-    } else if (mode === '6') {
-      if (instructions[number1] !== '0') {
-        position += 3;
-      } else {
-        position = parseInt(instructions[number2], 10);
-      }
-      continue;
-    } else if (mode === '7') {
-      if (parseInt(instructions[number1], 10) >= parseInt(instructions[number2], 10)) {
-        instructions[number3] = '0';
-      } else {
-        instructions[number3] = '1';
-      }
-    } else if (mode === '8') {
-      if (instructions[number1] !== instructions[number2]) {
-        instructions[number3] = '0';
-      } else {
-        instructions[number3] = '1';
-      }
-    } else if (mode === '1') {
-      instructions[number3] = (parseInt(instructions[number1], 10) + parseInt(instructions[number2], 10)).toString();
-    } else if (mode === '2') {
-      instructions[number3] = (parseInt(instructions[number1], 10) * parseInt(instructions[number2], 10)).toString();
-    }
-
-    position += positionObject[mode];
   }
 }
+
+console.log(solve(input));
