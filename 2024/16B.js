29b29193d865ca8d9d08085ba8a68fc1d592f6be
Chao Zhang <czhang@axonify.com>
2025-01-01 04:06:48 -0500

    Change 2024 day16

--- 2024/16B.js
+++ 2024/16B.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const input = fs.readFileSync(0, 'utf8').trim();
-const dmap = new Map([['N', [-1, 0]], ['S', [1, 0]], ['W', [0, -1]], ['E', [0, 1]], ['NE', [1, 1]], ['SE', [-1, 1]], ['SW', [-1, -1]], ['NW', [1, -1]], ['U', [-1, 0]], ['D', [1, 0]], ['L', [0, -1]], ['R', [0, 1]], ['v', [1, 0]], ['>', [0, 1]], ['^', [-1, 0]], ['<', [0, -1]]]);
+const dmap = new Map([['N', [-1, 0]], ['S', [1, 0]], ['W', [0, -1]], ['E', [0, 1]]]);
+const dstr = 'NESW';
 class Heap { constructor() { this.array = []; } peek() { return 0 === this.array.length ? null : this.array[0]; } poll() { if (0 === this.array.length) return null; if (1 === this.array.length) return this.array.pop(); const r = this.array[0]; return this.array[0] = this.array.pop(), this.heapifyDown(0), r; } add(r) { return this.array.push(r), this.heapifyUp(this.array.length - 1), this; } isEmpty() { return 0 == this.array.length; } heapifyUp(r) { let a = Math.floor((r - 1) / 2); for (; a >= 0 && !this.checkInvariant(this.array[a], this.array[r]);) [this.array[a], this.array[r]] = [this.array[r], this.array[a]], r = a, a = Math.floor((a - 1) / 2); } heapifyDown(r) { let a, t = 2 * r + 1, h = 2 * r + 2; for (; t < this.array.length && (a = h < this.array.length && this.checkInvariant(this.array[h], this.array[t]) ? h : t, !this.checkInvariant(this.array[r], this.array[a]));) [this.array[r], this.array[a]] = [this.array[a], this.array[r]], r = a, t = 2 * a + 1, h = 2 * a + 2; } checkInvariant(r, a) { return r[0] <= a[0]; } }
 const readnum = (a) => a.match(/\d+/g).map(a => Number(a));
 const readnum2d = (a) => a.split('\n').map(a => readnum(a));
@@ -12,7 +13,6 @@ function solve(input) {
   let arr = readword(input), R = arr.length, C = arr[0].length;
 
   let sr, sc, er, ec;
-  let dstr = 'NESW';
 
   for (let r = 0; r < R; r++)
     for (let c = 0; c < C; c++)
